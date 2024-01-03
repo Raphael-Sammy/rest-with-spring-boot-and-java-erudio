4d87043ff05fca6e20b22de23ab55ca3c5fddc04
Raphael-Sammy <133025924+Raphael-Sammy@users.noreply.github.com>
2024-01-03 18:02:36 -0300

    First Steps in Java with Spring Boot 3

---
+++ Application/src/test/java/rest/with/spring/boot/and/java/erudio/ApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.with.spring.boot.and.java.erudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
